75dd1d719f8e5529e6b1a938ad9fd9eddb38e7e9
OlBesp <olbesp365@gmail.com>
2017-09-28 14:09:52 +0300

    Make init() function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,52 @@
 var numOfSquares = 6;
-var colors = generateRandomColors(6);
+var colors = [];
+var pickedColor;
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
 
-for (var i = 0; i < modeButtons.length; i++) {
-	modeButtons[i].addEventListener("click", function() {
-		modeButtons[0].classList.remove("selected");
-		modeButtons[1].classList.remove("selected");
-		this.classList.add("selected");
-		this.textContent === "Easy" ? numOfSquares = 3: numOfSquares = 6;
-		reset();
-	});
+
+init();
+
+function init() {
+	setupModeButtons();
+	setupSquares();
+	reset();
+}
+
+function setupModeButtons() {
+	for (var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function() {
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent === "Easy" ? numOfSquares = 3: numOfSquares = 6;
+			reset();
+		});
+	}
+}
+
+function setupSquares() {
+	for (var i = 0; i < squares.length; i++) {
+		//add click listeners to squares
+		squares[i].addEventListener("click", function() {
+			//grab color of clicked square
+			var clickedColor = this.style.backgroundColor;
+			//compare color to pickedColor
+			if (clickedColor === pickedColor) {
+				//change all squares backgroung 
+				messageDisplay.textContent = "Correct!";
+				resetButton.textContent = "Play again?";
+				changeColors(clickedColor);
+			} else {
+				this.style.backgroundColor = "#232323";
+				messageDisplay.textContent = "Try again"
+			}
+		});
+	}
 }
 
 function reset() {
@@ -45,29 +76,6 @@ function reset() {
 resetButton.addEventListener("click", function() {
 	reset();
 });
-
-colorDisplay.textContent = pickedColor;
-
-for (var i = 0; i < squares.length; i++) {
-	//add colors to squares
-	squares[i].style.backgroundColor = colors[i];
-
-	//add click listeners to squares
-	squares[i].addEventListener("click", function() {
-		//grab color of clicked square
-		var clickedColor = this.style.backgroundColor;
-		//compare color to pickedColor
-		if (clickedColor === pickedColor) {
-			//change all squares backgroung 
-			messageDisplay.textContent = "Correct!";
-			resetButton.textContent = "Play again?";
-			changeColors(clickedColor);
-		} else {
-			this.style.backgroundColor = "#232323";
-			messageDisplay.textContent = "Try again"
-		}
-	});
-}
 
 function changeColors(color) {
 	//loop through all squares
